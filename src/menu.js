86de4f46a91426af05694c8b772f266e6bcb08c3
Matthew Halliday <matthalliday54@hotmail.com>
2023-07-19 19:26:39 +0100

    Add list of sandwiches

--- src/menu.js
+++ src/menu.js
@@ -7,12 +7,32 @@ const drawMenu = () => {
     const sandwiches = document.createElement("h2");
     sandwiches.classList.add("sub-head");
     sandwiches.textContent = "Sandwiches";
+    
     const sandwich_div = document.createElement("div");
     sandwich_div.classList.add("sandwiches");
+    const sandlist = document.createElement("ul");
+    const steak = document.createElement("li");
+    steak.textContent = "Steak and onions";
+    const blt = document.createElement("li");
+    blt.textContent = "BLT";
+    const caesar = document.createElement("li");
+    caesar.textContent = "Chicken Caesar";
+    const tuna = document.createElement("li");
+    tuna.textContent = "Tuna Mayonnaise";
+    const mozza = document.createElement("li");
+    mozza.textContent = "Mozzarella, Tomato and Basil";
 
+    sandlist.appendChild(steak);
+    sandlist.appendChild(blt);
+    sandlist.appendChild(caesar);
+    sandlist.appendChild(tuna);
+    sandlist.appendChild(mozza);
+
+    sandwich_div.appendChild(sandlist);
+    
     content.appendChild(title);
     content.appendChild(sandwiches);
-    content.appendChild(sandwiches_div);
+    content.appendChild(sandwich_div);
 
 }
 
