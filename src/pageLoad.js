80d9b47cef1c8310c18187b1d2c89523b46e7221
Matthew Halliday <matthalliday54@hotmail.com>
2023-07-19 11:53:03 +0100

    Set up skeleton files, and drawHome module

---
+++ src/pageLoad.js
@@ -0,0 +1,24 @@
+import { ContextExclusionPlugin } from "webpack";
+
+function drawHome = () => {    
+    const content = document.querySelector("div#content");
+
+    const home = document.createElement("div")
+    home.classList.add("home");
+    const copy = document.createElement("p");
+    const headline = document.createElement("h1")
+    const sandwich = document.createElement("img")
+
+
+    headline.textContent = "Matt's gourmet sandwiches";
+    copy.textContent = "The most extraordinary sandwich emporium creating the most delectable lunchtime food for the hungry masses of this nation.";
+    sandwich.setAttribute("src", "../img/sandwich.jpg");
+
+    home.appendChild(sandwich);
+    home.appendChild(headline);
+    home.appendChild(copy);
+    content.appendChild(home);
+}
+
+export {drawHome};
+
