547178f942c9519aeae35d2416c1f9a15069d0ef
Matthew Halliday <matthalliday54@hotmail.com>
2023-07-22 17:52:43 +0100

    Add mail icon on contact page

--- src/contact.js
+++ src/contact.js
@@ -6,21 +6,23 @@ const drawContact = () => {
     const title = document.createElement('h1');
     title.classList.add('title');
     title.textContent = "Contact";
-    const subhead = document.createElement('h2');
-    subhead.classList.add('sub-head');
-    subhead.textContent = "Address";
 
     const address = document.createElement('div');
+    const mail = document.createElement('img');
+    mail.setAttribute("src", "../img/mail_FILL0_wght400_GRAD0_opsz48.svg");
     address.classList.add('address');
-    address.setAttribute('style', 'white-space: pre;')
-    address.textContent = '32A Brailsford Rd\nLondon\nSW2 2TE';
+    const address_writing = document.createElement('p');
+    address_writing.setAttribute('style', 'white-space: pre;')
+    address_writing.textContent = '32A Brailsford Rd\nLondon\nSW2 2TE';
+
 
     const complaints = document.createElement('p')
     complaints.textContent = "Any complaints or fan-mail? Hound us, please"
 
-    
+    address.appendChild(mail);
+    address.appendChild(address_writing);
+ 
     container.appendChild(title);
-    container.appendChild(subhead);
     container.appendChild(address);
     container.appendChild(complaints);
     content.appendChild(container);
--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import './styles.css';
 import { drawContact } from "./contact.js";
 
 
-drawMenu();
+drawContact();
 
 const menu_tab = document.querySelector('#menu');
 const home_tab = document.querySelector('#home');
