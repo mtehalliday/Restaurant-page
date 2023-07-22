c51346f4eebbe02c5c56c2013e2e541e5f5c2773
Matthew Halliday <matthalliday54@hotmail.com>
2023-07-22 18:11:59 +0100

    Embed google iframe location (badly)

--- src/contact.js
+++ src/contact.js
@@ -17,7 +17,15 @@ const drawContact = () => {
 
 
     const complaints = document.createElement('p')
+    complaints.classList.add('complaints');
     complaints.textContent = "Any complaints or fan-mail? Hound us, please"
+
+    const map_container = document.createElement('div');
+    map_container.classList.add("map_container");
+    const map = document.createElement('iframe');
+    map.classList.add('map');
+    map.setAttribute('src', 'https://www.google.com/webhp?igu=1');
+    map.setAttribute('src', 'https://www.google.com/search?q=32a+brailsford+rd&igu=1&source=hp&ei=Ng28ZJnREeqphbIP6-2U4Ao&iflsig=AD69kcEAAAAAZLwbRivtf9FluX1yYbtAv3L7n4t5FFnB&oq=32A&gs_lp=Egdnd3Mtd2l6IgMzMkEqAggAMgQQIxgnMggQABiKBRiRAjIKEC4YigUYsQMYQzIQEAAYgAQYFBiHAhixAxiDATINEAAYgAQYFBiHAhixAzILEAAYgAQYsQMYgwEyBRAAGIAEMgUQABiABDIFEAAYgAQyBRAAGIAESKocULUIWMgNcAJ4AJABAZgBngGgAa4CqgEDMi4xuAEDyAEA-AEBqAIKwgIHECMY6gIYJ8ICERAuGIAEGLEDGIMBGMcBGNEDwgIKEAAYigUYsQMYQ8ICCBAAGIAEGLED&sclient=gws-wiz#');
 
     address.appendChild(mail);
     address.appendChild(address_writing);
@@ -25,6 +33,11 @@ const drawContact = () => {
     container.appendChild(title);
     container.appendChild(address);
     container.appendChild(complaints);
+    
+    map_container.appendChild(map);
+    container.appendChild(map_container);
+
+
     content.appendChild(container);
 }
 
